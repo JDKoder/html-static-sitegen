576fb26084fb2211803dd58bcc460312e86c671e
JDKoder <jacob.krieger@gmail.com>
2025-07-18 09:41:59 -0400

    Adding LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,10 +6,6 @@ class HTMLNode:
         self.props = props
 
 
-    def to_html(self):
-        raise NotImplementedError("Not Implemented")
-
-
     def props_to_html(self):
         prop_str = ""
         if self.props and len(self.props)>0:
@@ -20,6 +16,17 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+    def to_html(self):
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, ltag=None, lvalue=None, lprops=None):
+        if lvalue == None:
+            raise ValueError("Leaf nodes must have a value")
+        super().__init__(tag=ltag, value=lvalue, props=lprops)
 
 
 """node = HTMLNode(tag="a", value="foo", props={"href":"https://www.foo.com"})
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode 
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,9 +17,15 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         self.assertEqual("", node.props_to_html())
 
+
     def test_htmlnode_repr(self):
         node = HTMLNode(tag="a", value="Google", props={"href":"http://www.google.com", "target":"_blank"})
         self.assertEqual(node.__repr__(), f"HTMLNode({node.tag}, {node.value}, {node.children}, {node.props})")
+
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 
 if __name__ == "__main__":
