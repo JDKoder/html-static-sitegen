e388f141642c549c2964af6076ce0ee47b341ef6
Jacob Krieger <jboy@debee>
2025-08-18 16:17:54 -0400

    Implemented converting entire md document to html wrapped in div tags

--- src/htmlconverter.py
+++ src/htmlconverter.py
@@ -1,26 +1,26 @@
+import re
 from splitnodes import split_doc_to_blocks, text_to_nodes
 from blockconverter import block_to_block_type
 from textconverter import text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from textnode import TextType
+from blocktype import BlockType
 
 
 # converts an md text document into an html text document wrapped in <div></div> tags.
 def markdown_to_html_node(doc):
     #todo, put this in its own file
+    html_nodes = []
     blocks = split_doc_to_blocks(doc)
     for block in blocks:
         #determine type
         block_type = block_to_block_type(block);
-        block_node = None;
+        html_node = None;
         match (block_type):
-            case TextType.TEXT:
+            case BlockType.PARAGRAPH:
                 text_nodes = text_to_nodes(block)
                 block_html_children = text_to_children(text_nodes)
-                block_node = ParentNode("p", block_html_children)
-                break
-            case TextType.HEADING:
-                # TODO: special handling:  count the number of "#" marks up to 6
+                html_node = ParentNode("p", block_html_children)
+            case BlockType.HEADING:
                 hnum = 0;
                 while hnum < 7:
                     hnum += 1
@@ -29,22 +29,39 @@ def markdown_to_html_node(doc):
                 }
                 text_nodes = text_to_nodes(block[hnum:len(block)].strip())
                 block_html_children = text_to_children(text_nodes)
-                block_node = ParentNode(f"h{hnum}", block_html_children)
-                break
-            case TextType.CODE:
-                # TODO special handling:  wrap with <pre></pre> tags and treet block as one long text node without the wrapping ```...``` ie. do not add inner html
-                block_node = ParentNode("pre", [LeafNode("code", block[3:len(block)-3])])
-                break
-            case TextType.QUOTE:
+                html_node = ParentNode(f"h{hnum}", block_html_children)
+            case BlockType.CODE:
+                # wrap with <pre></pre> and treat block as one long text node without the wrapping ```...``` ie. do not add inner html
+                html_node = ParentNode("pre", [LeafNode("code", block[3:len(block)-3])])
+            case BlockType.QUOTE:
                 #remove all > characters that follow a newline
-                split_block = block[1:len(block)]).split("\n>")
+                split_block = block[1:len(block)].split("\n>")
+                #Replace the newline characters without the '>'
                 formatted_block = "\n".join(split_block)
+                #Now that the wrapping markdown is removed, we can conver the text into nodes
                 text_nodes = text_to_nodes(formatted_block)
                 block_html_children = text_to_children(text_nodes)
-                
-                block_node = ParentNode("blockquote", block_html_children)
-                break
-
+                html_node = ParentNode("blockquote", block_html_children)
+            case ORDERED_LIST:
+                #remove all - characters
+                split_block = re.split("\n[1-9][0-9]*[.]", block[2:len(block)])
+                #create parent nodes out of each of these splits
+                ol_children = []
+                for split in split_block:
+                    ol_children.append(ParentNode('li', text_to_children(text_to_nodes(split))))
+                html_node = ParentNode("ol", ol_children)
+            case UNORDERED_LIST:
+                #remove all - characters
+                split_block = re.split("\n-", block[1:len(block)])
+                #create parent nodes out of each of these splits
+                ol_children = []
+                for split in split_block:
+                    ol_children.append(ParentNode('li', text_to_children(text_to_nodes(split))))
+                html_node = ParentNode("ul", ol_children)
+            case _:
+                printf(f"unsupported case {block_type}")
+        html_nodes.append(html_node)
+    return ParentNode("div", html_nodes)
 
 
 #Takes a list of text nodes and converts them into html leaf nodes
