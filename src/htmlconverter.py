632ef7c2b13434deca94a92de4fd82a004ffd8e3
Jacob Krieger <jboy@debee>
2025-08-18 14:01:08 -0400

    Implemented Code, Text, and Quote types for html converter.

--- src/htmlconverter.py
+++ src/htmlconverter.py
@@ -1,8 +1,9 @@
-from splitnodes import split_doc_to_blocks 
+from splitnodes import split_doc_to_blocks, text_to_nodes
 from blockconverter import block_to_block_type
-from htmlnode import HTMLNode, LeafNode, ParentNode        
+from textconverter import text_node_to_html_node
+from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextType
-        
+
 
 # converts an md text document into an html text document wrapped in <div></div> tags.
 def markdown_to_html_node(doc):
@@ -14,5 +15,42 @@ def markdown_to_html_node(doc):
         block_node = None;
         match (block_type):
             case TextType.TEXT:
-                block_node = HTMLNode("p", block, None, None )#todo implement adding children
- 
+                text_nodes = text_to_nodes(block)
+                block_html_children = text_to_children(text_nodes)
+                block_node = ParentNode("p", block_html_children)
+                break
+            case TextType.HEADING:
+                # TODO: special handling:  count the number of "#" marks up to 6
+                hnum = 0;
+                while hnum < 7:
+                    hnum += 1
+                    if block[hnum] != "#":
+                        break
+                }
+                text_nodes = text_to_nodes(block[hnum:len(block)].strip())
+                block_html_children = text_to_children(text_nodes)
+                block_node = ParentNode(f"h{hnum}", block_html_children)
+                break
+            case TextType.CODE:
+                # TODO special handling:  wrap with <pre></pre> tags and treet block as one long text node without the wrapping ```...``` ie. do not add inner html
+                block_node = ParentNode("pre", [LeafNode("code", block[3:len(block)-3])])
+                break
+            case TextType.QUOTE:
+                #remove all > characters that follow a newline
+                split_block = block[1:len(block)]).split("\n>")
+                formatted_block = "\n".join(split_block)
+                text_nodes = text_to_nodes(formatted_block)
+                block_html_children = text_to_children(text_nodes)
+                
+                block_node = ParentNode("blockquote", block_html_children)
+                break
+
+
+
+#Takes a list of text nodes and converts them into html leaf nodes
+def text_to_children(text_nodes)
+    leaf_html_nodes = []
+    for text_node in text_nodes:
+        leaf_html_nodes.append(text_node_to_html_node(text_node))
+    return leaf_html_nodes
+        
