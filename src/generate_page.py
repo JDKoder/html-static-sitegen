a949dac2c32155eb821c252b4736e282b33ea5fc
JDKoder <jacob.krieger@gmail.com>
2025-08-22 10:12:02 -0400

    adding generate_page.py

---
+++ src/generate_page.py
@@ -0,0 +1,11 @@
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    #Read Markdown file @ from_path
+
+    #Read Template file @ template_path
+
+    #Use markdown_to_html_node and .to_html() method to convert the markdown file to an HTML string
+
+    #Use the extract_title function to grab the title of the page.
+
+    #Replace the {{ Title }} and {{ Content }} placeholder
