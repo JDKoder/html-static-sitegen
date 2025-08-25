7d4485e0ad9ee2b5e9ddebb0a8f18d74198eefdb
Jacob Krieger <jboy@debee>
2025-08-25 15:38:36 -0400

    Adding generate_pages_recursive to pull data out of the content directory recursively and copy it to the same directory structure in public.

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,6 +1,27 @@
 import os
+import shutil
 from htmlconverter import markdown_to_html_node
 from extractmd import extract_title
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dir_path_content):
+        raise ValueError(f"{dir_path_content} does not exist.")
+    if not os.path.exists(template_path):
+        raise ValueError(f"{template_path} does not exist.")
+
+    for file in os.listdir(dir_path_content):
+        working_file = f"{dir_path_content}/{file}"
+        print(f"Working on {working_file}")
+        if os.path.isdir(working_file):
+            print(f"{file} is a directory")
+            generate_pages_recursive(working_file, template_path, dest_dir_path + "/" + file)
+        else:
+            file_tuple = os.path.splitext(file)
+            print(f"file tuple is {file_tuple}")
+            # destination file should be named .html
+            new_filename = f"{file_tuple[0]}.html"
+            #print(f"file name is {file_tuple[0]} and will be saved as {new_filename}")
+            generate_page(working_file, template_path, f"{dest_dir_path}/{new_filename}")
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -29,7 +50,6 @@ def generate_page(from_path, template_path, dest_path):
     template_title_replaced = replace_tag("Title", html_template_file, title)
     final_result_html = replace_tag("Content", template_title_replaced, result_html)
     #Ensure directory exists prior to writing our file to it
-    
     #Even though this is a fun solution, we should not reinvent the wheel eh?
     #dest_dir = dest_path[0:len(dest_path)-(dest_path[::-1].index("/"))]
     dest_dir = os.path.dirname(dest_path)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from textnode import TextNode, TextType
 from static_copy import copy_to_dest_dir
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 
 def main():
     print(f"Transferring content from ./static to ./public")
     print(f"Removing directory ./public")
     copy_to_dest_dir("./static", "./public", overwrite=True)
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
+    #generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
 main()
